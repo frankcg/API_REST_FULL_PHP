1417769f2ac01d3d87970e84240d3d3197cf32ef
Frank Laura Borja <frank.cg9@gmail.com>
2018-10-25 17:49:58 -0500

    backup

---
+++ backup.sql
@@ -0,0 +1,36 @@
+/*
+SQLyog Ultimate v10.3 
+MySQL - 5.5.5-10.1.28-MariaDB : Database - java
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`java` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish2_ci */;
+
+USE `java`;
+
+/*Table structure for table `people` */
+
+DROP TABLE IF EXISTS `people`;
+
+CREATE TABLE `people` (
+  `id` int(5) unsigned NOT NULL AUTO_INCREMENT,
+  `name` char(100) COLLATE utf8_spanish2_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+/*Data for the table `people` */
+
+insert  into `people`(`id`,`name`) values (4,'Aquiles brinco'),(5,'Aquiles brinco'),(7,'Aquiles brinco'),(8,'Aquiles brinco'),(9,'Aquiles brinco'),(10,'Aquiles brinco'),(11,'Aquiles brinco'),(12,'Aquiles brinco'),(13,'Aquiles brinco'),(14,'frank laura'),(15,'frank laura'),(16,'frank 1'),(17,'frank 2'),(18,'frank 1'),(19,'frank 2');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
